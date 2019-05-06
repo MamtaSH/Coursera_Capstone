bcc5fb7247c5fb6c4ba485934a641a466ffedbab
MamtaSH <50255941+MamtaSH@users.noreply.github.com>
2019-05-05 22:47:40 -0400

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# This Notebook demonstrate specificalli Coursera capstone project
+
+# In[1]:
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+print("Hello Capstone Project Course!")
+
